d1edf6602191668d279c9bb41d715e45d7c99006
Maple <277804432@qq.com>
2024-10-08 23:41:07 +0800

    10月8日Chapter第一次修正

--- project/backend/server/src/main/java/org/frosty/demo/controller/ChapterController.java
+++ project/backend/server/src/main/java/org/frosty/demo/controller/ChapterController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.frosty.demo.config.CommonConstant;
 import org.frosty.demo.entity.dto.Chapter;
 import org.frosty.demo.services.ChapterService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,21 +17,17 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ChapterController {
 
-    @Autowired
-    private ChapterService chapterService;
+    private final ChapterService chapterService;
 
     // 创建章节
     @PostMapping("/chapter")
     public Map<String,String> createChapter(@RequestBody Chapter chapter) {
-        try {
             chapterService.createChapter(chapter);
             return Map.of("say","Successfully created course.");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 
     // 获取章节
+    // ？？？理论上来说，前端是通过接受用户的点击或者其他的请求来向后端要数据吧，那么我们还有必要去判断这个chapter是否真实存在吗？毕竟能出现在前端的chapter是不是都会在数据库中？
     @GetMapping("/chapter/{id}")
     public Chapter getChapter(@PathVariable Long id) {
         try {
--- project/backend/server/src/main/java/org/frosty/demo/entity/dto/Chapter.java
+++ project/backend/server/src/main/java/org/frosty/demo/entity/dto/Chapter.java
@@ -1,11 +1,16 @@
 package org.frosty.demo.entity.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.time.OffsetDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Chapter {
     private Long chapter_id;
     private Long course_id;
@@ -13,4 +18,5 @@ public class Chapter {
     private String chapter_type;
     private String content;
     private OffsetDateTime created_at;
+    private OffsetDateTime updated_at;
 }
