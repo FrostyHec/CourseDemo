f749b121b75fd41838221f3b9dbdfd4ac839a293
Maple <277804432@qq.com>
2024-10-09 11:08:41 +0800

    10月9日Resource占位

--- project/backend/server/src/main/java/org/frosty/demo/entity/dto/Chapter.java
+++ project/backend/server/src/main/java/org/frosty/demo/entity/dto/Chapter.java
@@ -4,7 +4,6 @@ package org.frosty.demo.entity.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 import java.time.OffsetDateTime;
 
---
+++ project/backend/server/src/main/java/org/frosty/demo/entity/dto/Resource.java
@@ -0,0 +1,20 @@
+package org.frosty.demo.entity.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Resource {
+    private Long resourceId; // 资源ID
+    private Long chapterId; // 章节ID
+    private String resourceName; // 资源名称
+    private String suffix; // 资源后缀
+    private String resourceVersionName; // 资源版本名称
+    private Integer resourceVersionOrder; // 资源版本顺序
+    private String resourceType; // 资源类型 (description/courseware/video/attachment)
+    private Boolean studentCanDownload; // 学生是否可以下载
+}
