f5c61cb28d063d3a306ce0794e4e1b51292ee74c
Forster <112929809+FrostyHec@users.noreply.github.com>
2024-10-11 12:45:59 +0800

    update a template

--- project/frontend/coursedemo/src/mockjs/MockUserAPI.ts
+++ project/frontend/coursedemo/src/mockjs/MockUserAPI.ts
@@ -3,12 +3,13 @@ import { backend_base } from '@/utils/Cosntant'
 import { setMockFunc } from '@/utils/MockUtils'
 import { APIResult, RequestType } from '@/utils/APIUtils'
 
-export function mockUserLoginCall(data: APIResult<LoginResult>) {
+export function mockLoginCall(data: APIResult<LoginResult>) {
   const url = backend_base + '/auth/login'
   setMockFunc(url, RequestType.POST, null, () => data)
 }
 
-export function mockUserLogoutCall() {
+export function mockLogoutCall() {
   const url = backend_base + '/auth/logout'
   setMockFunc(url, RequestType.POST, null, () => null)
 }
+
