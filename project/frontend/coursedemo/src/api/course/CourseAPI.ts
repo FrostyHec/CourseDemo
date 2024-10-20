99fdf424daa41979b4cd899b807ad884b3ed7422
Forster <112929809+FrostyHec@users.noreply.github.com>
2024-10-20 23:06:02 +0800

    update apis

--- project/frontend/coursedemo/src/api/course/CourseAPI.ts
+++ project/frontend/coursedemo/src/api/course/CourseAPI.ts
@@ -71,43 +71,6 @@ export async function searchCourseCall(pageSize: number, pageNum: number, name: 
   })
 }
 
-export async function studentEnrollCourseCall(courseId:number){
-  const url = backend_base+'/course/'+courseId+'/student/enroll'
-  return AxiosAPI.authPost<null>(url,{})
-}
-export async function teacherInviteStudentToCourseCall(courseId:number){
-  const url = backend_base+'/course/'+courseId+'/teacher/invite'
-  return AxiosAPI.authPost<null>(url,{})
-}
-
-export async function getAllStudentList(courseId:number,pageNum:number,pageSize:number){
-  const url = backend_base+'/course/'+courseId+'/student'
-  return AxiosAPI.authGet<{ content:StudentInfoWithEnrollStatus[] }>(url,{page_size:pageSize,page_num:pageNum,})
-}
-export async function getAllJoinedCourseList(studentId:number,pageNum:number,pageSize:number){
-  const url = backend_base+'/student/'+studentId+'/courses'
-  return AxiosAPI.authGet<{content:CourseEntity[]}>(url,{page_size:pageSize,page_num:pageNum,})
-}
-export async function getAllTeachingCourseList(studentId:number,pageNum:number,pageSize:number){
-  const url = backend_base+'/teacher/'+studentId+'/courses'
-  return AxiosAPI.authGet<{ content:CourseEntity[] }>(url,{page_size:pageSize,page_num:pageNum,})
-}
-
-export async function getStudentEnrollStatus(courseId:number,studentId:number,pageNum:number,pageSize:number){
-  const url = backend_base+'/course/'+courseId+'/student/'+studentId+'/status'
-  return AxiosAPI.authGet<{ status:StudentEnrollType }>(url,{page_size:pageSize,page_num:pageNum,})
-}
-export async function getAllPendingApprovedCourse(adminId:number,pageNum:number,pageSize:number){
-  const url = backend_base+'/admin/'+adminId+"/courses/submitted"
-  return AxiosAPI.authGet<{ content:CourseEntity[] }>(url,{page_size:pageSize,page_num:pageNum,})
-}
-export interface StudentInfoWithEnrollStatus extends UserPublicInfoEntity{
-  status:StudentEnrollType
-}
-export enum StudentEnrollType{
-  publik = 'publik',
-  invited = 'invited'
-}
 
 
 /////////////////////   RESOURCES   ///////////////////////////////
---
+++ project/frontend/coursedemo/src/api/course/CourseMemberAPI.ts
@@ -0,0 +1,42 @@
+import { backend_base } from '@/utils/Cosntant'
+import { AxiosAPI } from '@/utils/APIUtils'
+import type { UserPublicInfoEntity } from '@/api/user/UserAPI'
+import type { CourseEntity } from '@/api/course/CourseAPI'
+
+export async function studentEnrollCourseCall(courseId:number){
+  const url = backend_base+'/course/'+courseId+'/student/enroll'
+  return AxiosAPI.authPost<null>(url,{})
+}
+export async function teacherInviteStudentToCourseCall(courseId:number){
+  const url = backend_base+'/course/'+courseId+'/teacher/invite'
+  return AxiosAPI.authPost<null>(url,{})
+}
+
+export async function getAllStudentList(courseId:number,pageNum:number,pageSize:number){
+  const url = backend_base+'/course/'+courseId+'/student'
+  return AxiosAPI.authGet<{ content:StudentInfoWithEnrollStatus[] }>(url,{page_size:pageSize,page_num:pageNum,})
+}
+export async function getAllJoinedCourseList(studentId:number,pageNum:number,pageSize:number){
+  const url = backend_base+'/student/'+studentId+'/courses'
+  return AxiosAPI.authGet<{content:CourseEntity[]}>(url,{page_size:pageSize,page_num:pageNum,})
+}
+export async function getAllTeachingCourseList(studentId:number,pageNum:number,pageSize:number){
+  const url = backend_base+'/teacher/'+studentId+'/courses'
+  return AxiosAPI.authGet<{ content:CourseEntity[] }>(url,{page_size:pageSize,page_num:pageNum,})
+}
+
+export async function getStudentEnrollStatus(courseId:number,studentId:number,pageNum:number,pageSize:number){
+  const url = backend_base+'/course/'+courseId+'/student/'+studentId+'/status'
+  return AxiosAPI.authGet<{ status:StudentEnrollType }>(url,{page_size:pageSize,page_num:pageNum,})
+}
+export async function getAllPendingApprovedCourse(adminId:number,pageNum:number,pageSize:number){
+  const url = backend_base+'/admin/'+adminId+"/courses/submitted"
+  return AxiosAPI.authGet<{ content:CourseEntity[] }>(url,{page_size:pageSize,page_num:pageNum,})
+}
+export interface StudentInfoWithEnrollStatus extends UserPublicInfoEntity{
+  status:StudentEnrollType
+}
+export enum StudentEnrollType{
+  publik = 'publik',
+  invited = 'invited'
+}
