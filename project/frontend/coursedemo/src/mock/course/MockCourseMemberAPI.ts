6f79bf0d20b73303ad68430feb786c7d362f1d8e
Forster <112929809+FrostyHec@users.noreply.github.com>
2024-10-21 00:03:27 +0800

    update mock func

---
+++ project/frontend/coursedemo/src/mock/course/MockCourseMemberAPI.ts
@@ -0,0 +1,49 @@
+// Mock Functions
+import { backend_base } from '@/utils/Constant'
+import { setMockFunc } from '@/utils/MockUtils'
+import { APIResult, RequestType } from '@/utils/APIUtils'
+import type { StudentInfoWithEnrollStatus } from '@/api/course/CourseMemberAPI'
+import type { CourseEntity } from '@/api/course/CourseAPI'
+import type { StudentEnrollType } from '@/api/course/CourseMemberAPI'
+
+// Mock for studentEnrollCourseCall
+export function mockStudentEnrollCourseCall(data: APIResult<null>, courseId: number) {
+  const url = backend_base + '/course/' + courseId + '/student/enroll';
+  setMockFunc(url, RequestType.POST, null, () => data);
+}
+
+// Mock for teacherInviteStudentToCourseCall
+export function mockTeacherInviteStudentToCourseCall(data: APIResult<null>, courseId: number) {
+  const url = backend_base + '/course/' + courseId + '/teacher/invite';
+  setMockFunc(url, RequestType.POST, null, () => data);
+}
+
+// Mock for getAllStudentList
+export function mockGetAllStudentList(data: APIResult<{ content: StudentInfoWithEnrollStatus[] }>, courseId: number) {
+  const url = backend_base + '/course/' + courseId + '/student';
+  setMockFunc(url, RequestType.GET, null, () => data);
+}
+
+// Mock for getAllJoinedCourseList
+export function mockGetAllJoinedCourseList(data: APIResult<{ content: CourseEntity[] }>, studentId: number) {
+  const url = backend_base + '/student/' + studentId + '/courses';
+  setMockFunc(url, RequestType.GET, null, () => data);
+}
+
+// Mock for getAllTeachingCourseList
+export function mockGetAllTeachingCourseList(data: APIResult<{ content: CourseEntity[] }>, studentId: number) {
+  const url = backend_base + '/teacher/' + studentId + '/courses';
+  setMockFunc(url, RequestType.GET, null, () => data);
+}
+
+// Mock for getStudentEnrollStatus
+export function mockGetStudentEnrollStatus(data: APIResult<{ status: StudentEnrollType }>, courseId: number, studentId: number) {
+  const url = backend_base + '/course/' + courseId + '/student/' + studentId + '/status';
+  setMockFunc(url, RequestType.GET, null, () => data);
+}
+
+// Mock for getAllPendingApprovedCourse
+export function mockGetAllPendingApprovedCourse(data: APIResult<{ content: CourseEntity[] }>, adminId: number) {
+  const url = backend_base + '/admin/' + adminId + '/courses/submitted';
+  setMockFunc(url, RequestType.GET, null, () => data);
+}
---
+++ project/frontend/coursedemo/src/mock/course/MockCourseResourceAPI.ts
@@ -0,0 +1,35 @@
+// Mock Func
+import type { ResourceEntity } from '@/api/course/CourseResourceAPI'
+import { backend_base } from '@/utils/Constant'
+import { setMockFunc } from '@/utils/MockUtils'
+import { APIResult, RequestType } from '@/utils/APIUtils'
+
+// Mock 上传资源
+export function mockUploadResourceCall(chapterId: number, data: APIResult<null>) {
+  const url = `${backend_base}/chapter/${chapterId}/resource`;
+  setMockFunc(url, RequestType.POST, null, () => data);
+}
+
+// Mock 获取资源元数据
+export function mockGetResourceMetaDataCall(id: number, data: APIResult<ResourceEntity>) {
+  const url = `${backend_base}/resource/${id}`;
+  setMockFunc(url, RequestType.GET, null, () => data);
+}
+
+// Mock 更新资源元数据
+export function mockUpdateResourceMetadataCall(id: number, data: APIResult<null>) {
+  const url = `${backend_base}/resource/${id}`;
+  setMockFunc(url, RequestType.PUT, null, () => data);
+}
+
+// Mock 删除资源
+export function mockDeleteResourceCall(id: number, data: APIResult<null>) {
+  const url = `${backend_base}/resource/${id}`;
+  setMockFunc(url, RequestType.DELETE, null, () => data);
+}
+
+// Mock 获取章节下的所有资源
+export function mockGetResourcesByChapterCall(chapterId: number, data: APIResult<{ content: ResourceEntity[] }>) {
+  const url = `${backend_base}/chapter/${chapterId}/resource`;
+  setMockFunc(url, RequestType.GET, null, () => data);
+}
