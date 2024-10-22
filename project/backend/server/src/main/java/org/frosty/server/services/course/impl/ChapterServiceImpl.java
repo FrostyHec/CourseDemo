0c40266a4c39439a5dde6c468bb973ce7ba3e935
Maple <277804432@qq.com>
2024-10-22 23:43:35 +0800

    10.22chapter使用BaseMapper

--- project/backend/server/src/main/java/org/frosty/server/services/course/impl/ChapterServiceImpl.java
+++ project/backend/server/src/main/java/org/frosty/server/services/course/impl/ChapterServiceImpl.java
@@ -16,22 +16,22 @@ public class ChapterServiceImpl implements ChapterService {
 
     @Override
     public void createChapter(Chapter chapter) {
-        chapterMapper.insertChapter(chapter);
+        chapterMapper.insert(chapter);
     }
 
     @Override
     public Chapter findByID(Long id) {
-        return chapterMapper.getChapterById(id);
+        return chapterMapper.selectById(id);
     }
 
     @Override
     public void updateChapter(Long id, Chapter updatedChapter) {
-        chapterMapper.updateChapter(id, updatedChapter.getCourseId(), updatedChapter.getChapterTitle(), updatedChapter.getChapterType(), updatedChapter.getContent());
+        chapterMapper.updateById(updatedChapter);
     }
 
     @Override
     public void deleteChapter(Long id) {
-        chapterMapper.deleteChapterById(id);
+        chapterMapper.deleteById(id);
     }
 
     @Override
