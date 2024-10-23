2206fc6997a4f2db6f2a8764e109339b2856478a
Maple <277804432@qq.com>
2024-10-23 11:00:59 +0800

    10.23增加了对现有评论的评论支持

--- project/backend/server/src/main/java/org/frosty/server/entity/bo/ResourceComment.java
+++ project/backend/server/src/main/java/org/frosty/server/entity/bo/ResourceComment.java
@@ -19,6 +19,7 @@ public class ResourceComment {
     @TableId(type = IdType.AUTO)
     private Long commentId;
     private int resourceId;
+    private int preCommentId;
     private int userId;
     private String commentText;
     private OffsetDateTime createAt;
