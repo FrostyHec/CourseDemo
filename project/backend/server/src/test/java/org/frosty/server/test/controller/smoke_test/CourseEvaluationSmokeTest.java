b9fdb2f232c7f2208203b3caeda353ccff3c814c
Forster <112929809+FrostyHec@users.noreply.github.com>
2024-10-29 19:53:05 +0800

    fix search user

--- project/backend/server/src/test/java/org/frosty/server/test/controller/smoke_test/CourseEvaluationSmokeTest.java
+++ project/backend/server/src/test/java/org/frosty/server/test/controller/smoke_test/CourseEvaluationSmokeTest.java
@@ -1,5 +1,11 @@
 package org.frosty.server.test.controller.smoke_test;
 
 public class CourseEvaluationSmokeTest {
-    // TODO 有待细化
+    // TODO 基础测试
+    // 1. 学生对某个课程发送课程评价
+    // 2. 更新
+    // 3. 删除
+    // 4. 获取
+    // 需要注意:模拟测试时， private JsonNode evaluationFormAnswer; 这个应该可以支持传入并存储任意样式的json（后续会依据表单type检查）
+
 }
