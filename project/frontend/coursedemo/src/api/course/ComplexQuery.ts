4747273d273f8fd2041b995208bb0eccfa8d5736
Forster <112929809+FrostyHec@users.noreply.github.com>
2024-11-09 18:02:17 +0800

    update API

---
+++ project/frontend/coursedemo/src/api/course/ComplexQuery.ts
@@ -0,0 +1,8 @@
+import { service_backend_base } from '@/utils/Constant'
+import { AxiosAPI } from '@/utils/APIUtils'
+import type { CourseEntity } from '@/api/course/CourseAPI'
+
+export async function getAllStructureOfCourse(courseId:number) {
+  const url = service_backend_base + '/course'+courseId;
+  return AxiosAPI.authPost<null>(url,{})
+}
