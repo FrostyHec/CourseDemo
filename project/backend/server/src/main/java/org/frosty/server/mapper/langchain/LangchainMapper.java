4b3f8096cf9600e34f3ccc7fd03f983fea6e4e60
Maple <277804432@qq.com>
2024-11-17 18:19:00 +0800

    11.17文心连接，流式传输没有试过晚点验证

--- project/backend/server/src/main/java/org/frosty/server/mapper/langchain/LangchainMapper.java
+++ project/backend/server/src/main/java/org/frosty/server/mapper/langchain/LangchainMapper.java
@@ -12,16 +12,20 @@ import java.util.List;
 
 @Mapper
 public interface LangchainMapper extends BaseMapper<Assignment> {
+    // 不确定这个context这样写是否合法
+    @Update("UPDATE chat_history SET context = #{context,jdbcType=OTHER} WHERE id = #{id}")
+    void updateByChatId(Long id, LangchainController.ChatContext context);
 
-    @Update("")
-    void updateByChatId(Long id);
-
-    @Update("")
+    @Update("UPDATE chat_history SET title = #{title} WHERE id = #{id}")
     void setChatTitle(String title, Long id);
 
+    @Select("SELECT context FROM chat_history WHERE id = #{id}")
     List<LangchainController.SingleChatMessage> selectChatContentByChatId(Long id);
 
+    @Select("SELECT id, title, context, created_at, updated_at FROM chat_history WHERE id = #{id}")
     LangchainController.ChatEntity selectChatEntityByChatId(Long id);
 
+    @Select("SELECT id, title, created_at, updated_at FROM chat_history WHERE user_id = #{userID}")
     List<LangchainController.ChatEntity> selectChatListByUserId(long userID);
+
 }
