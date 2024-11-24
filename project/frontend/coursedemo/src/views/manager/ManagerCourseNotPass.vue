6c305f745d40f4c67256d82449aea7617267b21a
DrTeresia <12212226@mail.sustech.edu.cn>
2024-11-24 20:22:51 +0800

    update

--- project/frontend/coursedemo/src/views/manager/ManagerCourseNotPass.vue
+++ project/frontend/coursedemo/src/views/manager/ManagerCourseNotPass.vue
@@ -134,7 +134,7 @@ const checkCourse = (row: CourseEntity) => {
 };
 
 const approveCourse = async (row: CourseEntity) => {
-  const result = updateCourseStatusCall(row.course_id, {status:CourseStatus.published});
+  const result = await updateCourseStatusCall(row.course_id, {status:CourseStatus.published});
   if((await result).code == 200){
       fetchCourses;
   }
@@ -144,7 +144,7 @@ const approveCourse = async (row: CourseEntity) => {
 };
 
 const rejectCourse = async (row: CourseEntity) => {
-  const result = updateCourseStatusCall(row.course_id, {status:CourseStatus.rejected});
+  const result = await updateCourseStatusCall(row.course_id, {status:CourseStatus.rejected});
   if((await result).code == 200){
       fetchCourses;
   }
