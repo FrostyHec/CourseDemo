fb872bdd24e7db481db531a5e4c44f038b8d913c
Forster <112929809+FrostyHec@users.noreply.github.com>
2024-11-26 15:56:29 +0800

    add fix

--- project/backend/server/src/main/java/org/frosty/server/config/MyBatisPlusConfig.java
+++ project/backend/server/src/main/java/org/frosty/server/config/MyBatisPlusConfig.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.frosty.server.entity.bo.market.action_type.ActionParam;
+import org.frosty.server.entity.handler.JsonNodeTypeHandler;
 import org.frosty.server.utils.ActionParamTypeHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +23,9 @@ public class MyBatisPlusConfig {
 
     @Bean
     public ConfigurationCustomizer mybatisConfigurationCustomizer() {
-        return configuration -> configuration.getTypeHandlerRegistry().register(ActionParam.class, new ActionParamTypeHandler());
+        return configuration -> {
+            configuration.getTypeHandlerRegistry().register(ActionParam.class, new ActionParamTypeHandler());
+        configuration.getTypeHandlerRegistry().register(JsonNode.class, new JsonNodeTypeHandler());
+        };
     }
 }
