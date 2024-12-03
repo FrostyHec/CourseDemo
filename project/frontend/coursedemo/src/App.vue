dfd80c8a5a0856e12105e0f5c218f27be4bec2f5
DrTeresia <12212226@mail.sustech.edu.cn>
2024-12-03 23:37:46 +0800

    update

--- project/frontend/coursedemo/src/App.vue
+++ project/frontend/coursedemo/src/App.vue
@@ -2,4 +2,28 @@
   <el-config-provider namespace="ep">
     <RouterView></RouterView>
   </el-config-provider>
+  <el-dialog
+      title="退出登录"
+      v-model="quitVisible"
+      @close="router.push('/login')"
+    >
+      <span>另一个用户登录，您将被登出</span>
+      <template #footer>
+        <el-button type="primary" @click="handleLogout()">确认</el-button>
+      </template>
+    </el-dialog>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useEventStore } from "./stores/event";
+import { EventType } from "./utils/EventBus";
+import router from "./router";
+const quitVisible = ref(false);
+const {emitEvent} = useEventStore();
+
+const handleLogout = () =>{
+  emitEvent(EventType.currentlyIsLoggedOut);
+  router.push('/login');
+}
+</script>
