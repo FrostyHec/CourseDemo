2d8891720f0fe84c856df06ed8a97f8270ff1e0c
DrTeresia <12212226@mail.sustech.edu.cn>
2024-12-03 21:45:30 +0800

    update

--- project/frontend/coursedemo/src/views/Course/liveStream.vue
+++ project/frontend/coursedemo/src/views/Course/liveStream.vue
@@ -77,12 +77,12 @@ const danmus = ref<string[]>([]);
 
 onMounted(async () => {
   connectWebSocket();
+  if (authStore.user.role == UserType.STUDENT) {
+        showStream.value = false;
+  };
   console.log(videoElement.value);
   if (videoElement.value) {
     intervalId = setInterval(async () => {
-      if (authStore.user.role == UserType.STUDENT) {
-        showStream.value = false;
-      };
       const pullNameResponse = await getPullName(courseId);
       if (pullNameResponse && pullNameResponse.data) {
         streamName = pullNameResponse.data.name;
